b788847a5089a7c57b14d9cc5360e692df5d6304
most2820 <most2820@yandex.ru>
2022-02-22 18:20:19 +0300

    init

---
+++ out/res/resources.d
@@ -0,0 +1,4 @@
+out/res/resources.o: res/resources.res \
+E:/Sega/Mario/res/map.png \
+E:/Sega/Mario/res/jump.wav \
+E:/Sega/Mario/res/player.png
---
+++ res/resources.h
@@ -0,0 +1,10 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 sfx_jump[6144];
+extern const Palette palette_all;
+extern const TileSet bga_tileset;
+extern const SpriteDefinition mario;
+extern const MapDefinition bga_map;
+
+#endif // _RES_RESOURCES_H_
