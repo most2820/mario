4ee1a2ed445348de84764f3c958a1e12beeac40f
most2820 <most2820@yandex.ru>
2022-03-12 16:50:10 +0300

    fix collision

--- res/resources.h
+++ res/resources.h
@@ -4,7 +4,7 @@
 extern const u8 sfx_jump[6144];
 extern const Palette palette_all;
 extern const TileSet bga_tileset;
-extern const SpriteDefinition mario;
+extern const SpriteDefinition marioSprite;
 extern const MapDefinition bga_map;
 
 #endif // _RES_RESOURCES_H_
--- res/resources.res
+++ res/resources.res
@@ -1,7 +1,5 @@
 PALETTE palette_all "map.png"
 TILESET bga_tileset "map.png" BEST ALL
 MAP bga_map "map.png" bga_tileset 2 BEST 0
-
-SPRITE mario "player.png" 2 2 FAST 5
-
+SPRITE marioSprite "player.png" 2 2 FAST 5
 WAV sfx_jump "jump.wav" XGM
